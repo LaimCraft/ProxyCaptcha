c2d17a5451b19d6051cf5f488c7a1abf6a774452
isaevisa05 <isaevisao55@gmail.com>
2025-03-23 06:16:32 +0300

    Update

---
+++ src/main/java/com/github/isaevisa05/proxyсaptcha/ProxyCaptcha.java
@@ -0,0 +1,18 @@
+package com.github.isaevisa05.proxyсaptcha;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(id = "proxycaptcha", name = "ProxyCaptcha", version = "0.0.1", url = "https://www.LaimCraft.Ru", authors = {"LaimCraft", "isaevisa05"})
+public class ProxyCaptcha {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
