14f9604ca48104e5a797cdd089013b863cb37ddd
isaevisa05 <isaevisao55@gmail.com>
2025-03-24 18:07:56 +0300

    Update

--- src/main/java/com/github/isaevisa05/proxycaptcha/ProxyCaptcha.java
+++ src/main/java/com/github/isaevisa05/proxycaptcha/ProxyCaptcha.java
@@ -4,16 +4,28 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
 @Plugin(id = "proxycaptcha", name = "ProxyCaptcha", version = "0.0.1", url = "https://www.LaimCraft.Ru", authors = {"LaimCraft", "isaevisa05"})
 public class ProxyCaptcha {
 
-    @Inject
+    private ProxyServer server;
     private Logger logger;
+
+    @Inject
+    public ProxyCaptcha(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+
+        server.getCommandManager().unregister("server");
+        server.getCommandManager().unregister("velocity");
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         TunnelRunner tunnelRunner = new TunnelRunner(logger);
     }
+
+
 }
